b2cd70f4686f2ced6c505f52c59f911d0fa6e564
DESKTOP-V06510B\diogo <diogoqueirooz1212@gmail.com>
2018-05-29 01:21:39 -0300

    Add the api.js and created the list on the screen

--- src/App.js
+++ src/App.js
@@ -1,51 +1,72 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+import api from './Api'
+
 class App extends Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props)
     
-    this.state = {
-      count: 0
-    }
-  }
+		this.state = {
+      genres: [],
+      isLoading: false
+		}
+	}
   
-  componentDidMount(props) {
-    axios.get('http://localhost:8080/genres').then((res) => console.log(res))
-  }
-  render() {
-    return (
-      <div className="App">
-        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
-          <div className="container">
-            <div className="navbar-header page-scroll">
-              <a className="navbar-brand page-scroll" href="#page-top">
-                <img src="images/logo.png" height="30" />
-              </a>
-            </div>
-            <div className="collapse navbar-collapse navbar-ex1-collapse">
-              <ul className="nav navbar-nav">
-                <li>
-                  <a href="">Menu item {this.state.count}</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+	componentDidMount(props) {
+    this.setState({
+      isLoading: true
+    })
+    api.loadGenres()
+      .then((res) => this.setState({
+        isLoading: false,
+        genres: res.data
+      }))
+	}
+	render() {
+		return (
+			<div className="App">
+				<nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+					<div className="container">
+						<div className="navbar-header page-scroll">
+							<a className="navbar-brand page-scroll" href="#page-top">
+								<img src="images/logo.png" height="30" />
+							</a>
+						</div>
+						<div className="collapse navbar-collapse navbar-ex1-collapse">
+							<ul className="nav navbar-nav">
+								<li>
+									<a href="">Menu item {this.state.count}</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
       
-        <section id="intro" className="intro-section">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <h1><img src="images/logo.png" /></h1>
-                <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
-              </div>
-            </div>
-          </div>
+				<section id="intro" className="intro-section">
+					<div className="container">
+						<div className="row">
+							<div className="col-lg-12">
+								<h1><img src="images/logo.png" /></h1>
+								<p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+							</div>
+						</div>
+					</div>
+				</section>
+
+        <section>
+          {
+            this.state.isLoading &&
+            <span>Aguarde, carregando...</span>
+          }
+          {
+            !this.state.isLoading &&
+            <span>{JSON.stringify(this.state.genres)}</span>
+          }
         </section>
-      </div>
-    )
-  }
+			</div>
+		)
+	}
 }
 
 export default App
